4d695be20772648c135751fdb0fed230432324c6
paynezhuang <paynezhuang@gmail.com>
2024-05-05 22:43:21 +0800

    perf(projects): perf code

--- panis-boot-common/src/main/java/com/izpan/common/util/CollectionUtil.java
+++ panis-boot-common/src/main/java/com/izpan/common/util/CollectionUtil.java
@@ -9,11 +9,11 @@ import java.util.Collection;
  *
  * @Author payne.zhuang <paynezhuang@gmail.com>
  * @ProjectName panis-boot
- * @ClassName com.izpan.common.util.CollectionUtils
+ * @ClassName com.izpan.common.util.CollectionUtil
  * @CreateTime 2024/1/10 - 23:26
  */
 @SuppressWarnings("java:S2176")
-public class CollectionUtils extends org.springframework.util.CollectionUtils {
+public class CollectionUtil extends org.springframework.util.CollectionUtils {
 
     public static boolean isNotEmpty(@Nullable Collection<?> collection) {
         return !org.springframework.util.CollectionUtils.isEmpty(collection);
