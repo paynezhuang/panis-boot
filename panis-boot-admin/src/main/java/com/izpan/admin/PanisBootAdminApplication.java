bcbe4375349a594249ed4c2acd085aacc358cedc
paynezhuang <5275445@qq.com>
2024-11-27 14:57:11 +0800

    style(projects): format code

--- panis-boot-admin/src/main/java/com/izpan/admin/PanisBootAdminApplication.java
+++ panis-boot-admin/src/main/java/com/izpan/admin/PanisBootAdminApplication.java
@@ -1,10 +1,8 @@
 package com.izpan.admin;
 
-import com.izpan.infrastructure.util.SpringUtil;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
